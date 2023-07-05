89ec805e19922480fe0aa15ab77e0b53627171b9
Colin Huber <cbhuber@gmail.com>
2023-07-05 11:49:16 -0600

    feat: customer context provider hook.

---
+++ src/PostContext.js
@@ -0,0 +1,64 @@
+import { createContext, useContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+// 1. Create a new context
+const PostContext = createContext();
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    // 2. Provide value to child components
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        conClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+// Customer context provider hook
+function usePosts() {
+  const context = useContext(PostContext);
+
+  // This should not be used in the very top component (App()), as its supposed to pass this data directly to the children
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider.");
+  return context;
+}
+
+export { PostProvider, usePosts };
